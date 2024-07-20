a7f234f98b4165f00c9226741852466d9d13ed52
Benjamin Meadows <bemeadows@gmail.com>
2024-07-20 14:26:55 -0700

    change styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bg-primary": "#8DA7D8",
+        "bg-secondary": "#C2D8F0",
+        "text-main": "#303436",
+        "text-primary": "#FFFDFA",
+      },
+    },
   },
+  variants: {},
   plugins: [],
-}
+};
