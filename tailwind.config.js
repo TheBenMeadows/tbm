fa6773f8fa4f30ebeecd89bcb44df0420cf8e4b0
Benjamin Meadows <bemeadows@gmail.com>
2024-07-20 17:38:24 -0700

    revert icons and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,7 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {
-      colors: {
-        "bg-primary": "#8DA7D8",
-        "bg-secondary": "#C2D8F0",
-        "text-main": "#303436",
-        "text-primary": "#FFFDFA",
-      },
-    },
+    extend: {},
   },
-  variants: {},
   plugins: [],
 };
